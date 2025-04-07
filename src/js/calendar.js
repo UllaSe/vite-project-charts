2c1a46136e7b030d82fed7daf4f2db6935f2ce2d
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2025-04-07 15:22:46 +0300

    add vanillajs calender

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -12,6 +12,54 @@ document.querySelector('#app').innerHTML = `Moi ${localStorage.getItem('nimi')}`
 
 // https://vanilla-calendar.pro/docs/learn/installation-and-usage
 
+const handleClickDate = (self) => {
+	console.log(self.context);
+	//console.log('Tee jotain kivaa, päivämäärä:', self.context.selectedDates);
+};
+
+const decoratePrices = (self, dateEl) => {
+	const randomBoolean = Math.random() < 0.5;
+	if (!randomBoolean) return;
+	const randomPrice = Math.floor(Math.random() * (999 - 100 + 1) + 100);
+	const btnEl = dateEl.querySelector('[data-vc-date-btn]');
+	console.log(btnEl);
+	if (!btnEl) return;
+	const day = btnEl.innerText;
+	btnEl.style.flexDirection = 'column';
+	btnEl.innerHTML = `
+	  <span>${day}</span>
+	  <span style="font-size: 8px; color: #8BC34A;">$${randomPrice}</span>
+	`;
+};
+
+const options = {
+	type: 'default',
+	locale: 'fi-FI',
+	popups: {
+		'2025-04-08': {
+			modifier: 'hrv-circle',
+			html: `<div data-hrv=55 data-note="Flight to Vegas">
+      ✈️ Flight </div>`,
+		},
+		'2025-04-09': {
+			html: `<div>
+        <u><b>3:00 PM</b></u>
+        <p style="margin: 5px 0 0;">Meeting with team</p>
+      </div>`,
+			customProps: {
+				workDay: true,
+				notes: 'Team Sync-up',
+				id: 'id-3456',
+			},
+		},
+	},
+	onClickDate: handleClickDate,
+	onCreateDateEls: decoratePrices,
+};
+
+const calendar = new Calendar('#calendar', options);
+calendar.init();
+
 // FULL CALENDAR
 ///////////////////
 
