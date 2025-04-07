cbf2382a0c6662528636b09dfacedc7ceef73e06
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2025-04-07 10:37:03 +0300

    clean project

--- src/js/kubios.js
+++ src/js/kubios.js
@@ -1,11 +1,9 @@
 import '../css/style.css';
 import '../css/snackbar.css';
 import Chart from 'chart.js/auto';
-import {getUserInfo, getUserData} from './kubios-data.js';
+import { getUserInfo, getUserData } from './kubios-data.js';
 
-document.querySelector(
-  '#app',
-).innerHTML = `Moi kirjaantunut käyttäjä ${localStorage.getItem('nimi')}`;
+document.querySelector('#app').innerHTML = `Moi ${localStorage.getItem('nimi')}`;
 
 const getUserInfoBtn = document.querySelector('.get_user_info');
 getUserInfoBtn.addEventListener('click', getUserInfo);
@@ -13,26 +11,28 @@ getUserInfoBtn.addEventListener('click', getUserInfo);
 const getUserDataBtn = document.querySelector('.get_user_data');
 getUserDataBtn.addEventListener('click', getUserData);
 
-// piirretään graafit
+// piirretään graafit, suora esimerkki sivuilta
+// https://www.chartjs.org/docs/latest/getting-started/
+
 const ctx = document.getElementById('myChart');
 
 new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
+	type: 'bar',
+	data: {
+		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+		datasets: [
+			{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
 });
